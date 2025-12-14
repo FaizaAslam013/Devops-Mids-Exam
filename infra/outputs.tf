ac26578239ce77219f4b7f97d5430e2a2ecdac82
Noor Fatima <fa22-bcs-165@cuilahore.edu.pk>
2025-12-14 20:15:33 +0500

    Update outputs.tf for alraedy existing issue

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,9 +6,14 @@ output "public_subnet_id" {
   value = aws_subnet.public.id
 }
 
-output "security_group_id" {
+/*output "security_group_id" {
   value = aws_security_group.sg.id
+}*/
+
+output "security_group_id" {
+  value = data.aws_security_group.existing_sg.id
 }
+
 
 output "ec2_public_ip" {
   value = aws_instance.app.public_ip
