e99302bdfe4d61218dcb773fd7805bdb5d03ff6d
Noor Fatima <fa22-bcs-165@cuilahore.edu.pk>
2025-12-14 19:39:38 +0500

    Update outputs.tf for default vpc and limit issue

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,29 @@
-output "vpc_id" {
+/*output "vpc_id" {
   value = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
   value = aws_subnet.public.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.sg.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.app.public_dns
+}
+*/
+output "vpc_id" {
+  value = data.aws_vpc.default.id
+}
+
+output "public_subnet_id" {
+  value = data.aws_subnets.default.ids[0]
 }
 
 output "security_group_id" {
